7ee701a26e62c50074b6fd734f503fb388fe0dff
Kateryna <moskkat40@gmail.com>
2024-08-16 14:53:11 +0200

    make build

---
+++ index.140e379f.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".field-text");e.forEach(function(e){var t=document.createElement("label");t.classList.add("field-label"),t.setAttribute("for",e.id),t.textContent=e.name,e.before(t)}),e.forEach(function(e){e.placeholder=e.name.replace(e.name[0],e.name[0].toUpperCase())});
+//# sourceMappingURL=index.140e379f.js.map
